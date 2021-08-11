6d85f8239d28530420220a916ccfde7e827919f8
wangchuanyi <wang_cyi@163.com>
2021-08-11 21:24:46 +0800

    update

---
+++ leetcode/code/41.缺失的第一个正数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=cpp
+ *
+ * [41] 缺失的第一个正数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
