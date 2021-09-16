d29ebf76c44c203f7f97108fd2945ce8dc8a14d8
wangchuanyi <wang_cyi@163.com>
2021-09-16 11:09:43 +0800

    update

--- leetcode/code/22.括号生成.cpp
+++ leetcode/code/22.括号生成.cpp
@@ -8,16 +8,53 @@
 //https://leetcode-cn.com/problems/generate-parentheses/solution/hui-su-suan-fa-by-liweiwei1419/
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
+    vector<string> generateParenthesis1(int n) {
      //length:m=2*n ,m(m+1)/2
      //有效括号：i=3时候
      //())? left:1 right 2  not math
      //(()? left:2 right 1  may math
 
      vector<string> output;
-     string path =0;
+     string path;
+     dfs(path,0,0,n,output);
+     return output;
+    }
+
+    void dfs(string &path,int left,int right ,int n,vector<string> &output)
+    {
+        //condition 
+
+        if(left ==n && right ==n )
+        {
+            output.push_back(path);
+        }
+
+        //condition
+        if(right >left)
+        {
+            return ;
+        }
+
+        //condition
+        if (left <n)
+        {
+            path.push_back('(');
+            dfs(path,left+1,right,n,output);
+            path.pop_back();
+        }
+
+        //condition
+        if (right <n)
+        {
+            path.push_back(')');
+            dfs(path,left,right+1,n,output);
+            path.pop_back();
+        }
 
     }
+     //广度优先搜索。
+     vector<string> generateParenthesis1(int n) {
+     }
 };
 // @lc code=end
 
