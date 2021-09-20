a42c49a5080ddfebd7ce9b601e83c0ab42afed0a
wangchuanyi <wang_cyi@163.com>
2021-09-20 17:30:09 +0800

    update

--- leetcode/code/23.合并k个升序链表.cpp
+++ leetcode/code/23.合并k个升序链表.cpp
@@ -23,42 +23,82 @@ public:
      *     //02 初始化数组链表
      * */
 
-    ListNode *mergeKLists(vector<ListNode *> &lists)
+    ListNode *mergeKLists1(vector<ListNode *> &lists)
     {
-   
+
         auto comp = [&](ListNode *a, ListNode *b) {
             return a->val > b->val;
         };
 
-      priority_queue<ListNode*, vector<ListNode *>, decltype(comp)> pq(comp);
-       
-      ListNode head;//排序后链表固定头节点 
-      ListNode* ptail =&head;//
+        priority_queue<ListNode *, vector<ListNode *>, decltype(comp)> pq(comp);
 
-      for(ListNode* list:lists)
-        {  
-            if(list)
+        ListNode head;           //排序后链表固定头节点
+        ListNode *ptail = &head; //
+
+        for (ListNode *list : lists)
+        {
+            if (list)
             {
                 pq.push(list);
             }
         }
 
-       //03 循环迭代
-        while(!pq.empty())
+        //03 循环迭代
+        while (!pq.empty())
         {
-            ListNode* pcur = pq.top();
+            ListNode *pcur = pq.top();
             pq.pop();
 
             if (pcur->next)
             {
-                pq.push(pcur->next); 
-            }//该链表遍历完毕 邻接节点
+                pq.push(pcur->next);
+            } //该链表遍历完毕 邻接节点
 
             //04 链表tail插入操作
-             ptail->next = pcur;
-             ptail =pcur;
+            ptail->next = pcur;
+            ptail = pcur;
         }
-       return head.next;
+        return head.next;
+    }
+
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        // 小根堆的回调函数
+        auto comp = [&](ListNode *a, ListNode *b) {
+            return a->val > b->val;
+        };
+        priority_queue<ListNode *, vector<ListNode *>, decltype(comp)> pq(comp);
+
+        //在链表后面追加 需要头节点
+        ListNode head;
+        ListNode *ptail = &head;
+
+        //建立大小为k的小根堆
+        for (ListNode *ptemp : lists)
+        {
+            if (ptemp)
+            {
+                pq.push(ptemp);
+            }
+        }
+
+        while (!pq.empty())
+        {
+            //01 get
+            ListNode *pcur = pq.top();
+            pq.pop();
+            //02 push
+            if (pcur && pcur->next)
+            {
+                pd.push(pcur->next);
+            }
+
+            //03  在链表最后位置追加，不是前面添加。
+            //pcur->next =ptail->next;
+            ptail->next = pcur;
+            ptail = pcur;
+        }
+        return head.next;
     }
 };
 // @lc code=end
