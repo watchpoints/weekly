9932511f7f4a6d144e2f4a56e09d7496b9781336
wangchuanyi <wang_cyi@163.com>
2021-09-25 10:30:04 +0800

    update

---
+++ leetcode/code/513.找树左下角的值.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=cpp
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+
+    }
+};
+// @lc code=end
+
+/**
+ 
+## 思路描述：
+
+- 思路：
+看到这个题目马上dfs遍历，但是
+
+**/
